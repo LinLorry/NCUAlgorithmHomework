c1397096f92c4decbea80c43678496921903a3a0
LinLorry <lin864464995@163.com>
2020-06-19 21:44:47 +0800

    :loud_sound: Add quick sort log.

--- include/quit_sort.h
+++ include/quit_sort.h
@@ -1,14 +1,13 @@
 #ifndef NCU_ALGORITHM_HOMEWOKR_QUIT_SORT
 #define NCU_ALGORITHM_HOMEWOKR_QUIT_SORT
 
-#include <iostream>
 #include <vector>
 
 typedef std::vector<int> array_type;
 
 namespace ncu_algorithm_homework
 {
-    void quick_sort(array_type &array, int p, int r);
+    void quick_sort(array_type &array);
 } // namespace ncu_algorithm_homework
 
 #endif
--- src/quick_sort/quick_sort.cpp
+++ src/quick_sort/quick_sort.cpp
@@ -1,41 +1,74 @@
 #include <quit_sort.h>
+#include <iostream>
+#include <fstream>
+#include <cmath>
 
 using ncu_algorithm_homework::quick_sort;
-using std::cin;
 using std::cout;
 using std::endl;
-using std::vector;
+
+static const long long n = 10000L, count = 5;
+static const int count_max_length = 11;
 
 int main()
 {
-    size_t number;
-    array_type array;
+    array_type arrays[n];
 
-    cout << "Please input the number of array: ";
-    cin >> number;
+    clock_t startTime, endTime;
+    std::ofstream fout("log/quick_sort.log", std::ios_base::out | std::ios_base::app);
+    time_t rawtime;
+    struct tm *info;
+    char time_str[80];
 
-    cout << "Please input the array element: ";
-    array = array_type(number);
-    for (size_t i = 0; i < number; i++)
+    srand((unsigned int)time(&rawtime));
+    info = localtime(&rawtime);
+    strftime(time_str, 80, "%Y-%m-%d %H:%M:%S", info);
+
+    fout << "------------------------------------" << endl;
+    fout << "Test time: " << time_str << endl;
+    fout << "Test number: " << n << endl;
+    fout << "------------------------------------" << endl;
+    fout.width(count_max_length);
+    fout << "Value number\tUse Time" << endl;
+
+    for (size_t i = 0; i < count; i++)
     {
-        cin >> array[i];
+        const int number = pow(10, i + 1);
+        for (size_t j = 0; j < n; j++)
+        {
+            for (size_t k = 0; k < number; k++)
+            {
+                arrays[j].push_back(rand());
+            }
+        }
+
+        startTime = clock();
+        for (size_t j = 0; j < n; j++)
+        {
+            quick_sort(arrays[j]);
+        }
+        endTime = clock();
+        fout.width(count_max_length);
+        cout.width(count_max_length);
+        fout << number << ":\t" << endTime - startTime << endl;
+        cout << number << ":\t" << endTime - startTime << endl;
+
+        for (size_t j = 0; j < n; j++)
+        {
+            arrays[j].clear();
+        }
     }
 
-    quick_sort(array, 0, number - 1);
-
-    cout << "Quick sort result: ";
-    for (size_t i = 0; i < number; i++)
-    {
-        cout << array[i] << " ";
-    }
-    cout << endl;
+    fout << "------------------------------------" << endl
+         << endl;
+    fout.close();
 
     return 0;
 }
 
 namespace ncu_algorithm_homework
 {
-    size_t partition(array_type &array, const int p, const int r)
+    static size_t partition(array_type &array, const int p, const int r)
     {
         const int x = array[p];
         int i = p, j = r + 1;
@@ -58,7 +91,7 @@ namespace ncu_algorithm_homework
         }
     }
 
-    void quick_sort(array_type &array, int p, int r)
+    static void quick_sort(array_type &array, int p, int r)
     {
         if (p < r)
         {
@@ -73,6 +106,11 @@ namespace ncu_algorithm_homework
             quick_sort(array, p, q - 1);
             quick_sort(array, q + 1, r);
         }
+    }
+
+    void quick_sort(array_type &array)
+    {
+        quick_sort(array, 0, array.size());
     }
 
 } // namespace ncu_algorithm_homework
