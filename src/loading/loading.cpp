3415ee2d13adc0ad6ea77df4970f3463430160d0
LinLorry <lin864464995@163.com>
2020-06-19 22:15:09 +0800

    :loud_sound: Add loading log.

--- src/loading/loading.cpp
+++ src/loading/loading.cpp
@@ -1,16 +1,15 @@
-#include <iostream>
 #include <cstring>
-#include <quit_sort.h>
-#include <loading.h>
 #include <cmath>
+#include <fstream>
+#include <iostream>
+#include <loading.h>
 
 using ncu_algorithm_homework::loading;
-using std::cin;
 using std::cout;
 using std::endl;
 
-#define n 1000
-#define count 5
+static const long long n = 10000L, count = 5;
+static const int count_max_length = 11;
 
 int comp(const void *left, const void *right)
 {
@@ -24,24 +23,38 @@ int main()
     bool *result[n];
 
     clock_t startTime, endTime;
-    long long useTime[count];
+    std::ofstream fout("log/loading.log", std::ios_base::out | std::ios_base::app);
+    time_t rawtime;
+    struct tm *info;
+    char time_str[80];
+
+    srand((unsigned int)time(&rawtime));
+    info = localtime(&rawtime);
+    strftime(time_str, 80, "%Y-%m-%d %H:%M:%S", info);
+
+    fout << "------------------------------------" << endl;
+    fout << "Test time: " << time_str << endl;
+    fout << "Test number: " << n << endl;
+    fout << "------------------------------------" << endl;
+    fout.width(count_max_length);
+    fout << "Value number\tUse Time" << endl;
 
     for (size_t i = 0; i < count; i++)
     {
+        const size_t number = pow(10, i + 1);
         for (size_t j = 0; j < n; j++)
         {
-            const size_t tmp = pow(10, i + 1);
-            array[j] = new int[tmp];
-            result[j] = new bool[tmp];
-            num[j] = tmp;
+            array[j] = new int[number];
+            result[j] = new bool[number];
+            num[j] = number;
             max[j] = 0;
-            for (size_t k = 0; k < tmp; k++)
+            for (size_t k = 0; k < number; k++)
             {
                 array[j][k] = rand() % 100 + 1;
                 max[j] += array[j][k];
                 result[j][k] = false;
             }
-            qsort(array[j], tmp, sizeof(int), comp);
+            qsort(array[j], number, sizeof(int), comp);
             max[j] -= rand() % (max[j] - array[j][0]);
         }
 
@@ -51,7 +64,10 @@ int main()
             loading(max[j], array[j], result[j], num[j]);
         }
         endTime = clock();
-        useTime[i] = endTime - startTime;
+        fout.width(count_max_length);
+        cout.width(count_max_length);
+        fout << number << ":\t" << endTime - startTime << endl;
+        cout << number << ":\t" << endTime - startTime << endl;
 
         for (size_t j = 0; j < n; j++)
         {
@@ -59,11 +75,9 @@ int main()
             delete result[j];
         }
     }
-
-    for (size_t i = 0; i < count; i++)
-    {
-        cout << i << ": " << useTime[i] << endl;
-    }
+    fout << "------------------------------------" << endl
+         << endl;
+    fout.close();
 }
 
 void ncu_algorithm_homework::loading(int max, int *w, bool *resutl, const unsigned int num)
