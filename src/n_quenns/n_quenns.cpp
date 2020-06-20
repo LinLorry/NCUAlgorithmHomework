728c91a599a601ba5bf6ad1fe57dfacda4c760dd
LinLorry <lin864464995@163.com>
2020-06-20 16:30:55 +0800

    :loud_sound: Add n quenns log.

--- src/n_quenns/n_quenns.cpp
+++ src/n_quenns/n_quenns.cpp
@@ -1,37 +1,53 @@
 #include <iostream>
+#include <fstream>
 #include <n_quenns.h>
 
 using ncu_algorithm_homework::n_quenns;
 using std::cout;
 using std::endl;
 
+static const long long n = 1000, count = 20;
+static const int count_max_length = 11;
+
 int main()
 {
-    const long long n = 1000, count = 20;
-    int *x;
-    clock_t startTime, endTime;
+    int x[20];
 
-    srand((unsigned int)time(NULL));
+    clock_t startTime, endTime;
+    std::ofstream fout("log/n_quenns.log", std::ios_base::out | std::ios_base::app);
+    time_t rawtime;
+    struct tm *info;
+    char time_str[80];
+
+    srand((unsigned int)time(&rawtime));
+    info = localtime(&rawtime);
+    strftime(time_str, 80, "%Y-%m-%d %H:%M:%S", info);
+
+    fout << "------------------------------------" << endl;
+    fout << "Test time: " << time_str << endl;
+    fout << "Test number: " << n << endl;
+    fout << "------------------------------------" << endl;
+    fout.width(count_max_length);
+    fout << "Value number\tUse Time" << endl;
 
     for (size_t i = 10; i < count; i++)
     {
-        x = new int[i];
-
         startTime = clock();
         for (size_t j = 0; j < n; j++)
         {
             n_quenns(i, x);
         }
         endTime = clock();
-        cout << i << ": " << endTime - startTime << endl;
-        for (size_t j = 0; j < i; j++)
-        {
-            cout << x[j] << " ";
-        }
-        cout << endl;
 
-        delete[] x;
+        fout.width(count_max_length);
+        cout.width(count_max_length);
+        fout << i << ":\t" << endTime - startTime << endl;
+        cout << i << ":\t" << endTime - startTime << endl;
     }
+
+    fout << "------------------------------------" << endl
+         << endl;
+    fout.close();
 
     return 0;
 }
